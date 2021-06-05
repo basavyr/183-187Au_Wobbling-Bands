dba33e4b37d42f046c702e81ce65c17ae2a13cb1
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-05 20:56:35 +0300

    improve the plot content and scaling

--- src/python/app/plot.py
+++ src/python/app/plot.py
@@ -4,18 +4,19 @@ import os
 
 class Plot_Maker:
     @staticmethod
-    def Create_Plot(plot_file, data_set, label):
+    def Create_Plot(plot_file, data_set, plot_label):
         x = [data_point[0] for data_point in data_set]
         y = [data_point[1] for data_point in data_set]
 
         fig, ax = plt.subplots()
 
-        plt.plot(x, y, '-or', label=r'energies')
+        plt.plot(x, y, '-or', label=r'$E_w$')
+        ax.set_title(f'Wobbling energies for {plot_label}')
         plt.xlabel(r'$I\ [\hbar]$')
         plt.ylabel(r'$E\ [MeV]$')
         ax.legend(loc='best')
-        plt.text(0.80, 0.20, f'{label}', horizontalalignment='center',
-                 verticalalignment='center', transform=ax.transAxes, fontsize=8)
+        plt.text(0.80, 0.20, f'{plot_label}', horizontalalignment='center',
+                 verticalalignment='center', transform=ax.transAxes, fontsize=11)
 
         fig.tight_layout()
         plt.savefig(plot_file, bbox_inches='tight', dpi=1200)
@@ -23,18 +24,14 @@ class Plot_Maker:
 
     @staticmethod
     def Create_Band_Plots(plot_file, data_set, plot_label):
-
-        print(plot_label)
-
         fig, ax = plt.subplots()
-
         band_counter = 1
 
         for data in data_set:
             x = [data_point[0] for data_point in data]
             y = [data_point[1] for data_point in data]
 
-            plt.plot(x, y, '-or', label=f'band-{band_counter}')
+            plt.plot(x, y, '-or', label=f'Band-{band_counter}')
             band_counter += 1
 
         plt.xlabel(r'$I\ [\hbar]$')
@@ -42,7 +39,7 @@ class Plot_Maker:
         ax.legend(loc='best')
         ax.set_title(f'Wobbling energies for {plot_label}')
         plt.text(0.80, 0.20, f'{plot_label}', horizontalalignment='center',
-                 verticalalignment='center', transform=ax.transAxes, fontsize=8)
+                 verticalalignment='center', transform=ax.transAxes, fontsize=11)
 
         plt.savefig(plot_file, bbox_inches='tight', dpi=400)
         fig.tight_layout()
