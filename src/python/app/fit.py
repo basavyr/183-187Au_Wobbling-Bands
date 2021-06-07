92ad05a09711b6f77b82c14376111bb05a518fad
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-07 12:52:47 +0300

    create mock fit test and compare with mathematica algo

--- src/python/app/fit.py
+++ src/python/app/fit.py
@@ -23,19 +23,31 @@ class Fit:
 class Mock_Fit:
 
     @staticmethod
-    def Fit(mock_ydata):
-        return 1
+    def Fit(mock_data, model):
+
+        xdata = np.asarray(mock_data[0])
+        ydata = np.asarray(mock_data[1])
+
+        fit_results = Fit.Data_Fit(model, xdata, ydata)
+
+        return fit_results
 
 
 def Main():
     true_params = [1, 2, 3]
 
+    model = energies.Energy_Formula.Energy1
+
     xdata = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    true_ydata = [energies.Energy_Formula.Energy1(
+    true_ydata = [model(
         x, true_params[0], true_params[1], true_params[2]) for x in xdata]
 
     mock_ydata = [-0.10266646127212908, 7.812282901372733, -3.2278135465579325, 21.443416309796348, 13.103181055729578,
                   38.902538519456996, 42.7913558921784, 41.13754701768447, 53.78640767067495, 91.0827080115583, 101.41853935050891, 114.99844409250798]
+
+    mock_data = [xdata, mock_ydata]
+    params = Mock_Fit.Fit(mock_data, model)[0]
+    print(params)
 
 
 if __name__ == '__main__':
