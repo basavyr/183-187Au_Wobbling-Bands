6a131630797ca7d0863cb62f493211ac941d13af
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-07 20:45:27 +0300

    band-1 data is extracted

--- src/python/app/workflow.py
+++ src/python/app/workflow.py
@@ -14,7 +14,20 @@ def Main():
         # generate y_data
         band_0_energies = [x[1] for x in band_0]
 
-        #second band
+        # second band
+        # first band
+        band_1 = isotope_data[1]
+        # generate x_data
+        band_1_spins = [x[0] for x in band_1]
+        # generate y_data
+        band_1_energies = [x[1] for x in band_1]
+
+        data_0 = [band_0_spins, band_0_energies]
+        data_1 = [band_1_spins, band_1_energies]
+
+        print(data_0)
+        print(data_1)
+
 
 if __name__ == '__main__':
     Main()
