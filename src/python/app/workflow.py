9a6440bb37e5788f53581c5ec25c1f50f1be0c81
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-07 20:44:12 +0300

    band-0 data is extracted

--- src/python/app/workflow.py
+++ src/python/app/workflow.py
@@ -6,13 +6,15 @@ def Main():
     for isotope in energies.Files.EXP_DATA_FILES:
         isotope_data = energies.Extract_Data.Get_Energies(isotope)
         print(f'Nucleus -> {isotope_data[2]}')
-        # generate xdata
-        spins_data = [x[0] for x in isotope_data[0]]
-        # generate ydata
-        energy_data = [x[1] for x in isotope_data[0]]
-        print(spins_data)
-        print(energy_data)
 
+        # first band
+        band_0 = isotope_data[0]
+        # generate x_data
+        band_0_spins = [x[0] for x in band_0]
+        # generate y_data
+        band_0_energies = [x[1] for x in band_0]
+
+        #second band
 
 if __name__ == '__main__':
     Main()
