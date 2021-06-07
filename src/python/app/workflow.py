544cf4037066125d6cb2ac7413399c0147485d7d
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-07 20:35:25 +0300

    executes data ingest within workflow

--- src/python/app/workflow.py
+++ src/python/app/workflow.py
@@ -3,7 +3,9 @@ import fit
 
 
 def Main():
-    return 1
+    for isotope in energies.Files.EXP_DATA_FILES:
+        isotope_data = energies.Extract_Data.Get_Energies(isotope)
+        print(f'Nucleus -> {isotope_data[2]}')
 
 
 if __name__ == '__main__':
