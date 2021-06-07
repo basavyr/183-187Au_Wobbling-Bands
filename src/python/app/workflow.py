e9011774f1ad957d60871764d8f6e3852e72bf1a
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-07 20:41:12 +0300

    retrieves the band-0 information within the workflow

--- src/python/app/workflow.py
+++ src/python/app/workflow.py
@@ -6,6 +6,12 @@ def Main():
     for isotope in energies.Files.EXP_DATA_FILES:
         isotope_data = energies.Extract_Data.Get_Energies(isotope)
         print(f'Nucleus -> {isotope_data[2]}')
+        # generate xdata
+        spins_data = [x[0] for x in isotope_data[0]]
+        # generate ydata
+        energy_data = [x[1] for x in isotope_data[0]]
+        print(spins_data)
+        print(energy_data)
 
 
 if __name__ == '__main__':
