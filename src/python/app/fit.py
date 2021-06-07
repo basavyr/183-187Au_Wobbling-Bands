2d27e954d1876b14ad2d7f87d81fba98c6713945
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-07 08:05:25 +0300

    make clean fit function

--- src/python/app/fit.py
+++ src/python/app/fit.py
@@ -5,26 +5,16 @@ import energies
 
 import plot
 
-p1 = 1
-p2 = 2
-p3 = 3
 
-x_data = np.arange(0, 10, 0.5)
-y_data = [energies.Energy_Formula.Energy1(x, p1, p2, p3) for x in x_data]
+class Fit:
 
-y_data = [x + np.random.randint(-2, 2) for x in y_data]
+    @staticmethod
+    def Data_Fit(model, x_data_exp, y_data_exp):
 
-xdata = np.asarray(x_data)
-ydata = np.asarray(y_data)
+        x_data = np.asarray(x_data_exp)
+        y_data = np.asarray(y_data_exp)
 
-params, covariance = curve_fit(energies.Energy_Formula.Energy1, xdata, ydata)
+        params, covariance = curve_fit(
+            model, x_data, y_data)
 
-idx = 0
-for p in params:
-    idx += 1
-    print(f'p{idx} -> {p}')
-
-p1_new, p2_new, p3_new = params
-x_data = np.arange(0, 10, 0.5)
-y_data_new = [energies.Energy_Formula.Energy1(
-    x, p1_new, p2_new, p3_new) for x in x_data]
+        return params, covariance
