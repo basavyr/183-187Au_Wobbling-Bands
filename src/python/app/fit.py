873fead59e9222a5c32636a3bda9fd77b2c4d18a
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-07 10:54:35 +0300

    add mock class for fitting random data

--- src/python/app/fit.py
+++ src/python/app/fit.py
@@ -18,3 +18,22 @@ class Fit:
             model, x_data, y_data)
 
         return params, covariance
+
+
+class Mock_Fit:
+    mock_xdata = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    mock_ydata = [-0.10266646127212908, 7.812282901372733, -3.2278135465579325, 21.443416309796348, 13.103181055729578,
+                  38.902538519456996, 42.7913558921784, 41.13754701768447, 53.78640767067495, 91.0827080115583, 101.41853935050891, 114.99844409250798]
+
+    @staticmethod
+    def Fit(mock_ydata):
+        return 1
+
+
+def Main():
+    mock_ydata = Mock_Fit.mock_ydata
+    params = Mock_Fit.Fit(mock_ydata)
+
+
+if __name__ == '__main__':
+    Main()
