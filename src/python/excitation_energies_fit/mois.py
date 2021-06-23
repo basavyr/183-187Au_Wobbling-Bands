5b23b19d39e93becab1103e46caf13676246c313
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-23 16:11:45 +0300

    add helper functions for the MOI class

--- src/python/excitation_energies_fit/mois.py
+++ src/python/excitation_energies_fit/mois.py
@@ -2,3 +2,13 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+
+class MOI:
+
+    @staticmethod
+    def InertiaFactor(MOI):
+        return 1.0 / (2.0 * MOI)
+
+    @staticmethod
+    def Radians(angle):
+        return angle * np.pi / 180.0
