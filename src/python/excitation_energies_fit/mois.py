22707a656f94b9a99f9eaf4d9ccb88458fe6f358
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-23 16:12:33 +0300

    add main class

--- src/python/excitation_energies_fit/mois.py
+++ src/python/excitation_energies_fit/mois.py
@@ -10,5 +10,9 @@ class MOI:
         return 1.0 / (2.0 * MOI)
 
     @staticmethod
-    def Radians(angle):
+    def Rad(angle):
         return angle * np.pi / 180.0
+
+
+if __name__=='__main__':
+    print('OK')
