4be48b5343473eef05a924dc846ed29557ca5487
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-23 16:20:36 +0300

    maps sin function to the list of tuples

--- src/python/excitation_energies_fit/mois.py
+++ src/python/excitation_energies_fit/mois.py
@@ -15,12 +15,15 @@ class MOI:
 
     @staticmethod
     def Irrotational(I0, gm):
+        sin_squared = lambda x: np.power(np.sin(x), 2)
         gm_rad = MOI.Rad(gm)
         PI3 = np.pi / 3.0
         SIN_ARGS = [gm_rad - 2 * PI3 * k for k in range(1, 4)]
-        return SIN_ARGS
+
+        pure_mois = list(map(sin_squared, SIN_ARGS))
+        return pure_mois
 
 
 if __name__ == '__main__':
-    mois=MOI.Irrotational(1, 1)
+    mois = MOI.Irrotational(1, 1)
     print(mois)
